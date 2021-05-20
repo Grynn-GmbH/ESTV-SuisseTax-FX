67e1ba5d6d7e1ffa26b0645fb13edc5ce5ab3473
al-chaudhari <chaudharia041@gmail.com>
2021-05-20 18:54:25 +0530

    added check method

--- estv_suissetax_fx/cron.py
+++ estv_suissetax_fx/cron.py
@@ -1,2 +1,15 @@
+import frappe
+import requests
+
+
+def restore():
+    frappe.log('runn successfully')
+    frappe.error_log('this is Error')
+
+
 def run_daily():
-    print("Running Daily")
+    restore()
+
+
+def run_monthly():
+    restore()
